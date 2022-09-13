9a63152d7975b327ba533eb412fb9b5949a17996
Getver <112408148+Getver@users.noreply.github.com>
2022-09-13 17:59:52 +0900

    Add files via upload

---
+++ 02_함수/02_functionbasic.py
@@ -0,0 +1,13 @@
+# 1. 함수 만들기
+# def 함수이름(매개변수):
+#   명령블록
+#   return 결과값
+def sum(a, b):
+    result = a + b
+    return (result)
+
+
+# 2. 함수 사용하기
+# 함수이름(인자 1, 인자 2)
+print(sum(1, 2))
+print(sum(3, 4))
