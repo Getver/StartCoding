234d5ff7f96897bbd6e5c03d250b120b30508775
Getver <112408148+Getver@users.noreply.github.com>
2022-09-13 17:59:05 +0900

    Add files via upload

---
+++ 01_실행파일/01_hello.py
@@ -0,0 +1,9 @@
+print("hello startcoding!")
+input()
+
+# python install pyinstaller 설치
+# pyinstaller 01\hello.py : 실행파일 만들었음
+# pyinstaller -F -1\hello.py : 실행파일 1개만 만들었음
+# build 파일은 지워져도됨, spec은 설정인데 지워도됨
+# hello.exe 가 실행파일임 
+# 주의사항 : 소스코드에 상대경로가 있다면, 실행파일로 변환했을 때를 고려하자
